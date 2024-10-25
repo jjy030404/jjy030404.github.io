32f229b5848931772509f0a060ba39d0c824da1c
jjy030404 <jungj350@newschool.edu>
2024-10-25 10:04:15 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,13 @@
 document.querySelectorAll('.interactive-area').forEach(area => {
     area.addEventListener('click', (event) => {
         const location = event.target.dataset.location;
-        alert(`You clicked on ${location}!`);
-        });
+        
+        // Remove 'selected' class from all areas before adding to the clicked one
+        document.querySelectorAll('.interactive-area').forEach(a => a.classList.remove('selected'));
+        
+        // Add 'selected' class to the clicked area
+        event.target.classList.add('selected');
+        
+        alert(`You selected ${location}`);
+    });
 });
