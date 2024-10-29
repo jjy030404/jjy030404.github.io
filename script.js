f48a242666962c2b960f625e319fbd3378ffd5c9
jjy030404 <jungj350@newschool.edu>
2024-10-29 15:30:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ document.addEventListener("mousemove", (event) => {
     const scrollThreshold = 100; // Distance from bottom in pixels to start scrolling
     const scrollSpeed = 5; // Adjust this to control scroll speed
 
-    // If mouse is near the bottom of the window, scroll down
     if (window.innerHeight - event.clientY < scrollThreshold) {
         window.scrollBy(0, scrollSpeed);
     }
@@ -13,13 +12,9 @@ document.querySelectorAll('.interactive-area').forEach(area => {
         const location = event.target.dataset.location;
         const link = event.target.dataset.link;
         
-        // Remove 'selected' class from all areas before adding to the clicked one
         document.querySelectorAll('.interactive-area').forEach(a => a.classList.remove('selected'));
-        
-        // Add 'selected' class to the clicked area
         event.target.classList.add('selected');
         
-        // Redirect to the corresponding HTML page
         if (link) {
             window.location.href = link;
         } else {
