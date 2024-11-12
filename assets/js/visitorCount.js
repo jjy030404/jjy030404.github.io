c2640f71c00a249003119cc3b22dc4de5e1ae361
jjy030404 <jungj350@newschool.edu>
2024-11-12 14:54:31 -0500

    Update visitorCount.js

--- assets/js/visitorCount.js
+++ assets/js/visitorCount.js
@@ -4,7 +4,7 @@ function updateVisitorCount() {
     fetch(visitCountUrl)
         .then(response => response.json())
         .then(data => {
-            document.getElementById('visitor-count').innerText = `당신은 ${data.value}번째 방문자입니다.`;
+            document.getElementById('visitor-count').innerText = `You are visitor number ${data.value}.`;
         })
         .catch(error => {
             console.error("Error fetching visitor count: ", error);
