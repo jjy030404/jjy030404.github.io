5df8701fac73ef24a1345e55b15235e2b3742f35
jjy030404 <jungj350@newschool.edu>
2024-11-12 04:21:49 -0500

    Update toggleSections.js

--- assets/js/toggleSections.js
+++ assets/js/toggleSections.js
@@ -1,9 +1,6 @@
 function toggleSection(sectionId) {
-    // Hide all sections first
     document.getElementById('about').style.display = 'none';
     document.getElementById('contact').style.display = 'none';
-
-    // Show the selected section
     const section = document.getElementById(sectionId);
     if (section) {
         section.style.display = 'block';
