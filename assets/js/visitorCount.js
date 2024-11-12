106ecad65b289cb9fa42f539bb178a3b9520f676
jjy030404 <jungj350@newschool.edu>
2024-11-12 16:49:30 -0500

    Update visitorCount.js

--- assets/js/visitorCount.js
+++ assets/js/visitorCount.js
@@ -1,9 +1,7 @@
-// API 키를 설정합니다.
 const apiKey = "ba75df87969e4317845092a817a697b3";
 const visitCountUrl = `https://api.ipgeolocation.io/ipgeo?apiKey=${apiKey}`;
 
 function updateVisitorCount() {
-    // LocalStorage에서 방문자 수를 가져옵니다.
     let visitorCount = localStorage.getItem("visitorCount");
 
     if (!visitorCount) {
@@ -15,7 +13,6 @@ function updateVisitorCount() {
                 return response.json();
             })
             .then(data => {
-                // 방문자 수가 없는 경우 1로 시작하여 LocalStorage에 저장
                 visitorCount = 1;
                 localStorage.setItem("visitorCount", visitorCount);
                 document.getElementById('visitor-count').innerText = `You are visitor number ${visitorCount}.`;
@@ -25,7 +22,6 @@ function updateVisitorCount() {
                 document.getElementById('visitor-count').innerText = "Visitor count is unavailable at the moment.";
             });
     } else {
-        // LocalStorage에 방문자 수가 있는 경우
         visitorCount = parseInt(visitorCount) + 1;
         localStorage.setItem("visitorCount", visitorCount);
         document.getElementById('visitor-count').innerText = `You are visitor number ${visitorCount}.`;
