dea9a94f6645c0ee0c38294061abd671f5c039f8
jjy030404 <jungj350@newschool.edu>
2024-11-12 13:49:09 -0500

    Update visitorCount.js

--- assets/js/visitorCount.js
+++ assets/js/visitorCount.js
@@ -1,15 +1,3 @@
-const COUNT_API_KEY = "jjy030404_github_button_clicks";
-
-fetch(`https://api.countapi.xyz/hit/${COUNT_API_KEY}`)
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('visitor-count').innerText = `You are visitor number ${data.value} today.`;
-    })
-    .catch(error => console.error("Error updating click count:", error));
-
-fetch(`https://api.countapi.xyz/get/${COUNT_API_KEY}`)
-    .then(response => response.json())
-    .then(data => {
-        document.getElementById('visitor-count').innerText = `Total clicks today: ${data.value}`;
-    })
-    .catch(error => console.error("Error fetching click count:", error));
+function cb(response) {
+    document.getElementById('visits').innerText = response.value;
+}
