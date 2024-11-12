b34bd9f3ad56177c2a99031bdb9764d576a943d3
jjy030404 <jungj350@newschool.edu>
2024-11-12 17:04:01 -0500

    Create 5th_ave.js

---
+++ 5th_ave.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", () => {
+    const title = document.querySelector(".title");
+    title.classList.add("animate-title");
+});
+
+function showMoreInfo() {
+    alert("5th Avenue isn’t just a shopping street; it’s a world-famous destination! You’ll find museums, restaurants, and even a library that’s a national landmark. So take your time to explore!");
+}
