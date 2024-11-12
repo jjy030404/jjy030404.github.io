455ad75083ae7d874b000a2b0305c2e0903f5aa4
jjy030404 <jungj350@newschool.edu>
2024-11-12 13:48:40 -0500

    Update animations.js

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -1,7 +1,4 @@
-function startJourney() {
-    const loadingMessage = document.getElementById('loading-message');
-    loadingMessage.innerText = "Calculating the quickest route through NYC traffic... 🚖";
-    setTimeout(() => {
-        window.location.href = "map.html";
-    }, 2000);
-}
+document.addEventListener('DOMContentLoaded', () => {
+    const introContainer = document.getElementById('intro-container');
+    introContainer.classList.add('fade-in');
+});
