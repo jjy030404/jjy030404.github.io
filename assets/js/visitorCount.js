59f8bb10ef88515776022e8111d76426ad4437e0
jjy030404 <jungj350@newschool.edu>
2024-11-12 14:53:50 -0500

    Update visitorCount.js

--- assets/js/visitorCount.js
+++ assets/js/visitorCount.js
@@ -1,3 +1,14 @@
-function cb(response) {
-    document.getElementById('visits').innerText = response.value;
+const visitCountUrl = "https://api.countapi.xyz/hit/jjy030404.github.io/visits";
+
+function updateVisitorCount() {
+    fetch(visitCountUrl)
+        .then(response => response.json())
+        .then(data => {
+            document.getElementById('visitor-count').innerText = `당신은 ${data.value}번째 방문자입니다.`;
+        })
+        .catch(error => {
+            console.error("Error fetching visitor count: ", error);
+        });
 }
+
+document.addEventListener('DOMContentLoaded', updateVisitorCount);
