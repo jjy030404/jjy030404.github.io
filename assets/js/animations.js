231ccc9c98c739ff239ae35c399c8895d33c8442
jjy030404 <jungj350@newschool.edu>
2024-11-12 15:31:41 -0500

    Update animations.js

--- assets/js/animations.js
+++ assets/js/animations.js
@@ -2,13 +2,6 @@ function startJourney() {
     const loadingMessage = document.getElementById('loading-message');
     loadingMessage.innerText = "Calculating the quickest route through NYC traffic... 🚖";
 
-    let clickCount = parseInt(localStorage.getItem('clickCount')) || 0;
-    clickCount++;
-
-    localStorage.setItem('clickCount', clickCount);
-
-    document.getElementById('visitor-count').innerText = `Button clicked ${clickCount} times`;
-
     setTimeout(() => {
         window.location.href = "map.html";
     }, 2000);
