9b6bda620c5b6d39486c89ad76beb55ce0efb154
jjy030404 <jungj350@newschool.edu>
2024-11-12 16:38:13 -0500

    Update visitorCount.js

--- assets/js/visitorCount.js
+++ assets/js/visitorCount.js
@@ -1,15 +1,15 @@
 const visitCountUrl = "https://api.countapi.xyz/hit/jjy030404.github.io/visits";
 
 function updateVisitorCount() {
-    fetch(visitCountUrl)
+    fetch(visitCountUrl, { mode: 'no-cors' })
         .then(response => response.json())
         .then(data => {
             document.getElementById('visitor-count').innerText = `You are visitor number ${data.value}.`;
         })
         .catch(error => {
             console.error("Error fetching visitor count: ", error);
+            document.getElementById('visitor-count').innerText = "Visitor count is unavailable at the moment.";
         });
 }
 
-// 페이지 로드 시 방문자 수 업데이트
 document.addEventListener('DOMContentLoaded', updateVisitorCount);
