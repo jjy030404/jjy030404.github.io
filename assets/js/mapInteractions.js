72e3ba71b3f638b4852ed4b3f45ea0cd328d2c39
jjy030404 <jungj350@newschool.edu>
2024-11-26 14:53:37 -0500

    Update mapInteractions.js

--- assets/js/mapInteractions.js
+++ assets/js/mapInteractions.js
@@ -11,7 +11,7 @@ const locations = [
     { name: "7th Ave", coords: [40.761937, -73.982155], description: "Blending fashion, food, and flair.", link: "7th_ave.html" },
     { name: "8th Ave", coords: [40.759990, -73.991562], description: "Dynamic and bustling with vibrant culture.", link: "8th_ave.html" },
     { name: "Lexington Ave", coords: [40.751881, -73.973268], description: "Boutiques and a mix of Uptown-Downtown vibes.", link: "lexington_ave.html" },
-    { name: "Canal St", coords: [40.719254, -74.000952], description: "A bustling street, gateway to Chinatown.", link: "canal_street.html" },
+    { name: "10th Ave", coords: [40.746439, -74.007394], description: "A perfect mix of urban serenity and stunning views.", link: "10th_ave.html" },
     { name: "Broadway", coords: [40.759256, -73.984482], description: "Theater district lights and unforgettable shows.", link: "broadway.html" },
     { name: "Madison Ave", coords: [40.774290, -73.963634], description: "High-end shops with an elegant touch.", link: "madison_ave.html" },
     { name: "Park Ave", coords: [40.753011, -73.978020], description: "Greenery meets glam on this storied avenue.", link: "park_ave.html" },
@@ -33,5 +33,4 @@ locations.forEach(location => {
     bounds.push(location.coords);
 });
 
-// Adjust map to show all markers
 map.fitBounds(bounds, { padding: [50, 50] });
