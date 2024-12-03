0b9fed4fab80a919f8665074b539d4a003458eae
jjy030404 <jungj350@newschool.edu>
2024-12-03 13:37:15 -0500

    Update 3rd_ave.js

--- 3rd_ave.js
+++ 3rd_ave.js
@@ -2,6 +2,7 @@ const sliderBall = document.querySelector('.slider-ball');
 const body = document.body;
 let isDragging = false;
 
+
 sliderBall.addEventListener('mousedown', (e) => {
     isDragging = true;
     e.preventDefault();
@@ -13,10 +14,11 @@ document.addEventListener('mousemove', (e) => {
         const rect = sliderContainer.getBoundingClientRect();
         let left = e.clientX - rect.left;
 
+    
         left = Math.max(0, Math.min(left, rect.width));
         sliderBall.style.left = `${left}px`;
 
-     
+  
         const tilt = ((left / rect.width) - 0.5) * 40;
         body.style.transform = `rotate(${tilt}deg)`;
     }
